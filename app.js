6d33944f6641e95c59aa65a9df1a534f5306a71a
Daniel Bogucki <daniel.bogucki14@gmail.com>
2022-01-04 18:16:32 +0100

    Added final clock functionality

--- app.js
+++ app.js
@@ -8,14 +8,23 @@ function setDate() {
     const seconds = now.getSeconds();
     const secondsDegrees = ((seconds / 60) * 360) + 90;
     secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    if (seconds == 0) {
+        secondHand.style.transition = 'all 0s';
+    }
     
     const minutes = now.getMinutes();
     const minutesDegrees = ((minutes / 60) * 360) + 90;
     minutesHand.style.transform = `rotate(${minutesDegrees}deg)`;
+    if (minutes == 0) {
+        minutesHand.style.transition = 'all 0s';
+    }
 
     const hours = now.getHours();
     const hoursDegrees = ((hours / 12) * 360) + 90;
     hoursHand.style.transform = `rotate(${hoursDegrees}deg)`;
+    if (hours == 0) {
+        hoursHand.style.transition = 'all 0s';
+    }
 }
 
 setInterval(setDate, 1000);
